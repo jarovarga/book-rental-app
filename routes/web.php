dde880511e53f95fc807623fb3638c7ed5c37e4f
Jaro Varga <jaro.varga@neio.sk>
2025-05-19 12:35:58 +0200

    Add routes for books and authors resource management.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::patch('/books/{book}/toggle-borrowed', [BookController::class, 'toggleBorrowed'])->name('books.toggleBorrowed');
+Route::post('books/{book}/toggle', [BookController::class, 'toggleBorrowed'])->name('books.toggle');
+
+Route::resource('books', BookController::class);
+Route::resource('authors', AuthorController::class);
